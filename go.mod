dfeb06dcfe2625c5562a649335f02ef7fd7a78b7
Bhavik Patel <bpatel5493@gmail.com>
2024-02-24 18:03:44 -0600

    started working on tests

--- go.mod
+++ go.mod
@@ -6,7 +6,13 @@ require (
 	github.com/google/uuid v1.3.1
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/mattn/go-sqlite3 v1.14.17
+	github.com/stretchr/testify v1.8.1
 	go.uber.org/zap v1.26.0
 )
 
-require go.uber.org/multierr v1.11.0 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ internal/data/reminders/storage_json_test.go
@@ -0,0 +1,48 @@
+package reminders_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/bhavik3210/reminders/reminders-api/internal/data/reminders"
+	"github.com/stretchr/testify/assert"
+)
+var(
+	testDBFile = "testDB.json"
+)
+
+func NewMockJsonStorage() (*reminders.JsonStorage, error) {
+	return &reminders.JsonStorage{
+		FileName: testDBFile,
+	}, nil
+}
+
+func setup() *reminders.JsonStorage {
+	s, err := NewMockJsonStorage()
+	if err != nil {
+		fmt.Printf("failed to create NewMockJsonStorage: %s", err)
+	}
+	return s
+}
+
+func cleanup() {
+	err := os.Remove(testDBFile) 
+	if err != nil {
+		fmt.Printf("failed to delete test json file %s", err)
+	}
+}
+
+func TestSimpleWritet (t *testing.T) {
+	s := setup()
+	r := reminders.Reminder{
+		Title: "test title",
+	}
+
+	err := s.Save(&r)
+	if err != nil {
+		t.Error("failed to save reminder: %w", err)
+	}
+
+	assert.Equal(t,nil,  err, "err is nil")
+}
